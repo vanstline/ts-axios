eec67f22d12cfd4fc5144736fbbfdbecfcd2cbd2
shilongfei <shilongfei@pinlor.com>
2019-12-05 23:17:58 +0800

    feat(Func reload):

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-import { type } from 'os'
-
 export type Method =
   | 'get'
   | 'GET'
@@ -26,8 +24,8 @@ export type AxiosRequestConfig = {
   timeout?: number
 }
 
-export type AxiosResponse = {
-  data: any
+export type AxiosResponse<T = any> = {
+  data: T
   status: number
   statusText: string
   headers: any
@@ -35,7 +33,7 @@ export type AxiosResponse = {
   request: any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {}
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
 
 export type IAxios = {
   message: string
@@ -51,23 +49,25 @@ export type IAxiosError = Error &
   }
 
 export interface Axios {
-  request(config: AxiosRequestConfig): AxiosPromise
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
-  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  post(url: string, config?: AxiosRequestConfig): AxiosPromise
+  post<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  put(url: string, config?: AxiosRequestConfig): AxiosPromise
+  put<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  patch(url: string, config?: AxiosRequestConfig): AxiosPromise
+  patch<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
 export interface AxiosInstance extends Axios {
-  (config: AxiosRequestConfig): AxiosPromise
+  <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+
+  <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
